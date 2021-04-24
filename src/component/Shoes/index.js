cd6a05c249afbb8c17c3acb756909c85a0f58c71
pleasematheus <matheus4k@gmail.com>
2021-04-24 12:33:55 -0400

    More pages added

---
+++ src/component/Shoes/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Shoes() {
+	return (
+		<View>
+			<Text>SHOES</Text>
+		</View>
+	)
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,78 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Image, StyleSheet, ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+
+import Shoes from '../../component/Shoes'
 
 export default function Home() {
 	return (
-		<View>
-			<Text
-				style={{
-					marginTop: 60,
-					fontSize: 25,
-					fontFamily: 'Inter_700Bold'
-				}}
-			>
-				Home
-			</Text>
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Image
+					style={styles.image}
+					source={require('../../assets/banner.png')}
+				/>
+				<View style={styles.textContainer}>
+					<Text style={styles.text}>TÊNIS</Text>
+					<Text style={[styles.text, { color: '#CECECF' }]}>•</Text>
+					<Text style={[styles.text, { color: '#CECECF' }]}>MASCULINO</Text>
+					<TouchableOpacity style={{
+						position: 'absolute',
+						right: 0,
+						alignSelf: 'center'
+					}}>
+						<MaterialIcons
+							name="filter-list"
+							size={24}
+							color="#000"
+						/>
+					</TouchableOpacity>
+				</View>
+			</View>
+
+			<View style={styles.line} />
+			
+			<ScrollView>
+				<Text style={styles.text}>LANÇAMENTOS</Text>
+
+				<View style={{
+					flexDirection: 'row',
+					justifyContent: 'space-around'
+				}}>
+					<Shoes />
+					<Shoes />
+				</View>
+
+			</ScrollView>
+
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		width: '100%',
+		backgroundColor: '#FFF'
+	},
+	header: {
+		marginBottom: 8
+	},
+	image: {
+		width: '100%'
+	},
+	textContainer: {
+		flexDirection: 'row',
+		marginVertical: '5%',
+		marginHorizontal: '5%'
+	},
+	text: {
+		fontFamily: 'Inter_700Bold',
+		fontSize: 26,
+		marginHorizontal: '1%'
+	},
+	line: {
+		borderBottomColor: '#D8D8D8',
+		borderBottomWidth: 2
+	}
+ })
