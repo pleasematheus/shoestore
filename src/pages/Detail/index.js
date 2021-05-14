90fb078a74b1b96dd13c5a3dbaaa223eef542366
pleasematheus <matheus4k@gmail.com>
2021-05-13 20:20:30 -0400

    Update details

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,12 +1,46 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity, ScrollView, StyleSheet} from 'react-native'
 
-export default function Home() {
+export default function Detail({ navigation }) {
+
+	navigation.setOptions({
+		headerTitle: 'Nike Downshifter 10'
+	})
+
 	return (
-		<View>
-			<Text>
-				Detalhe
-			</Text>
-		</View>
+		<ScrollView style={styles.container}>
+			<Image
+				source={require('../../assets/detail.png')}
+				style={styles.image}
+				resizeMode="cover"
+			/>
+			<View>
+				<View>
+					<Text style={[styles.title, {fontSize: 24}]}>
+						R$ 280,90
+					</Text>
+				</View>
+
+				<View opacity={0.3}>
+					<Text style={[styles.title, { fontSize: 30 }]}>
+						Nike Downshifter 10
+					</Text>
+				</View>
+			</View>
+		</ScrollView>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		width: '100%',
+		backgroundColor: '#FFF'
+	},
+	image: {
+		width: '100%'
+	},
+	title: {
+		fontFamily: 'Inter_700Bold'
+	}
+})
